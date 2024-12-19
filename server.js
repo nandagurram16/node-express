5eb9b30e1d7977406a6240091c9b7961373e9248
nandagurram16 <nandagurram168@gmail.com>
2024-12-19 11:07:49 +0530

    message

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app=express()
+
+app.get('/create',(req,resp)=>{
+    resp.send('create my server for running')
+})
+
+app.get('/nanda',(req,resp)=>{
+    resp.send('created get app')
+})
+
+app.listen(5000,(err)=>{
+      if (err) throw err
+      console.log(`server is running`)
+})
